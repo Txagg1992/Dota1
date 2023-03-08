7b3f2b9cae7d1dddce3c647012ff7784b9878660
Enoch Phetteplace <curiousaps@gmail.com>
2023-03-08 00:34:54 -0600

    Update build.gradle.kts files.

--- hero/ui-heroList/build.gradle.kts
+++ hero/ui-heroList/build.gradle.kts
@@ -1,58 +1,7 @@
-plugins {
-    id ("com.android.library")
-    kotlin("android")
+apply{
+    from("$rootDir/android-library-build.gradle")
 }
 
-android {
-    compileSdk = Android.compileSdk
-    buildToolsVersion = Android.buildTools
-
-    defaultConfig {
-        minSdk = Android.minSdk
-        targetSdk = Android.targetSdk
-
-        testInstrumentationRunner = "com.codingwithmitch.dotainfo.CustomTestRunner"
-    }
-
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-        //useIR = true
-    }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = Compose.composeVersion
-    }
-    packagingOptions {
-        resources {
-            exclude("META-INF/AL2.0")
-            exclude("META-INF/LGPL2.1")        }
-    }
-}
-
-dependencies {
-
-    implementation(AndroidX.coreKtx)
-    implementation(AndroidX.appCompat)
-    implementation(AndroidX.lifecycleVmKtx)
-
-    implementation(Compose.activity)
-    implementation(Compose.ui)
-    implementation(Compose.material)
-    implementation(Compose.tooling)
-    implementation(Compose.navigation)
-    implementation(Compose.hiltNavigation)
-
-    implementation(Google.material)
+dependencies{
 
 }
